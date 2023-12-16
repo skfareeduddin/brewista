dd5e6eec0cbb0b79588ce1f9d0bcf04674728ac7
Syed Khaja Fareeduddin <skfareeduddin5@gmail.com>
2023-12-17 01:28:49 +0530

    Created my_orders.dart.

---
+++ lib/screens/my_orders.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyOrderScreen extends StatelessWidget {
+  const MyOrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
